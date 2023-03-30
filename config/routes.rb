e71f2ca61618672122e4cf5ca8dc55b75a46653f
pius ssekweyama <ssekweyamapius@gmail.com>
2023-03-30 15:55:02 +0300

    foods

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root "recipes#index"
 
+  resources :users do
+    resources :foods, only: [:index, :show, :new, :create, :destroy]
+  end
+
   delete '/recipes/:id', to: 'recipes#destroy', as: 'recipe'
   resources :recipes, only: [:index, :show, :new, :create, :destroy]
 end
